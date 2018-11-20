5f9c1c72489516efa2d80e07f92ca7996918d915
Marlon Costa <marlon.costa@ieee.org>
2018-11-20 12:30:08 -0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-#Carrega a eurística
-heuristics = pd.read_csv("./data/heuristics.csv", header=0, index_col=0)
-
